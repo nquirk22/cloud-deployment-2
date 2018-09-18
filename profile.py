76b2bb4856acff9a02ee345a86a03c3f128da433
Nathan Quirk <nquirk22@gmail.com>
2018-09-18 02:11:33 -0400

    added documentation

--- profile.py
+++ profile.py
@@ -16,23 +16,28 @@ pc = portal.Context()
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
  
-# Add four XenVMs to the request.
-
+# Create local topology
 link = request.LAN("lan")
 
-for i in range(1, 5):
-    node = request.XenVM("node-" + str(i))
+# Add four XenVMs to the request.
+for i in range(1, 5):    
+    node = request.XenVM("node-" + str(i)) 
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+    
     iface = node.addInterface("if" + str(i))
     iface.component_id = "eth" + str(i)
+    
+    # assign unique IPs
     iface.addAddress(rspec.IPv4Address("192.168.1." + str(i), "255.255.255.0"))        
     link.addInterface(iface)
+    
+    # Install and execute a script that is contained in the repository.
+    node.addService(rspec.Execute(shell="sh", command="/local/repository/silly.sh"))
 
+    # if node-1, give it a public interface
     if i == 1:
         node.routable_control_ip = True
         
-# Install and execute a script that is contained in the repository.
-node.addService(rspec.Execute(shell="sh", command="/local/repository/silly.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
