c1daec49b5497329187c7d98deeec84ad4614168
Nathan Quirk <nquirk22@gmail.com>
2018-09-18 01:42:10 -0400

    fixed another import

--- profile.py
+++ profile.py
@@ -32,7 +32,7 @@ for i in range(1, 5):
         node.routable_control_ip = True
         
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+node.addService(rspec.Execute(shell="sh", command="/local/repository/silly.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
